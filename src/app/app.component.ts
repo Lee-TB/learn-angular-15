9a5a6c59612cf889df9d6a0137521459cdd9be5e
Duc Tran <tblyduc2412@gmail.com>
2023-04-06 08:28:48 +0700

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'learn-angular-15';
+export class AppComponent implements OnInit {
+  checked: boolean = true;
+
+  constructor() {
+    console.log('checked: ', this.checked);
+  }
+
+  ngOnInit(): void {
+    console.log('checked: ', this.checked);
+  }
+
+  logger(value: any) {
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
+import { AuthorDetailComponent } from './components/author-detail/author-detail.component';
+import { ToggleComponent } from './components/toggle/toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent,
+    ProgressBarComponent,
+    AuthorListComponent,
+    AuthorDetailComponent,
+    ToggleComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
